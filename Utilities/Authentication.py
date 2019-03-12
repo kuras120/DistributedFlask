8eafc0afaecc597ab2148d9de4c0963cdb43b6da
Wojtek <wojciech.kur96@gmail.com>
2019-03-12 18:39:43 +0100

    Change encryption method

--- Utilities/Authentication.py
+++ Utilities/Authentication.py
@@ -5,7 +5,7 @@ import hashlib
 class Authentication:
     @staticmethod
     def generate_auth_token(username, password):
-        token = hashlib.sha3_512(username.encode('utf-8')
+        token = hashlib.sha512(username.encode('utf-8')
                                  + password.encode('utf-8')
                                  + request.headers.get('User-Agent').encode('utf-8')).hexdigest()
         return token
